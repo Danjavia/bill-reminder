69e46ea1718ad4e0187f3e073b73239f2d20310d
Jorge Vergara <javebratt@gmail.com>
2016-08-09 17:42:25 -0500

    Creates a list of paid and unpaid bills

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,44 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import { BillCreatePage } from '../bill-create/bill-create';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { DateFormatPipe } from 'angular2-moment';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  selector: 'app',
+  pipes: [DateFormatPipe]
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  billList: FirebaseListObservable<any>;
+  constructor(public navCtrl: NavController, public af: AngularFire, public alertCtrl: AlertController) {
+    this.billList = af.database.list('/bills');
 
+  }
+
+  newBill(){
+    this.navCtrl.push(BillCreatePage);
+  }
+
+  payBill(billId: string) {
+    this.billList.update(billId, { paid: true });
+  }
+
+  promptPayment(billId: string) {
+    let alert = this.alertCtrl.create({
+      message: "Mark as paid?",
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Mark as Paid',
+          handler: data => {
+            this.payBill(billId);
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
