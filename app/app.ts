4702caf2415de4e5b032d790c6e6996a2f115923
Jorge Vergara <javebratt@gmail.com>
2016-08-09 14:37:40 -0500

    Adds AngularFire2 and Firebase to the app

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HomePage} from './pages/home/home';
+import { Component } from '@angular/core';
+import { Platform, ionicBootstrap } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+import { HomePage } from './pages/home/home';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 
 @Component({
@@ -19,4 +20,12 @@ export class MyApp {
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase({
+    apiKey: "AIzaSyCw1SKmz1Bu2ibncu5Bxd1sZgR1taV7ujM",
+    authDomain: "aftutorial-ef306.firebaseapp.com",
+    databaseURL: "https://aftutorial-ef306.firebaseio.com",
+    storageBucket: "aftutorial-ef306.appspot.com",
+  })
+]);
